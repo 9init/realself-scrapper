cf36a6b62432b260e845fc855f2ca25d504f1729
Ahmed Hesham <emovent2e@gmail.com>
2024-02-29 21:59:44 +0200

    fix paggination

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const config = yaml.load(fsAsync.readFileSync(configPath, 'utf8'));
 
 // Extract parameters from config
 const baseUrl = config.baseUrl;
+const scrapeUrl = config.scrapeUrl;
 const section = config.section;
 const startPage = config.startPage;
 const endPage = config.endPage;
+const bypassCache = config.bypassCache;
 const userAgent = config.userAgent;
 
 // Set the user agent in the headers
@@ -46,7 +48,7 @@ async function downloadImage(content) {
 
   // Download the image
   const imagePath = path.join(directoryPath, fileName);
-  
+
   const imageStream = fsAsync.createWriteStream(imagePath, { flags: 'w' });
 
   try {
@@ -63,15 +65,14 @@ async function downloadImage(content) {
 }
 
 // Function to scrape a specific page and section
-async function scrapePage(pageNumber) {
-  const url = `${baseUrl}${section}#page=${pageNumber}`;
-
+async function scrapePage(topicId, pageNumber) {
+  const url = `${scrapeUrl}?bypass_cache=${bypassCache}&page=${pageNumber}&topic_id=${topicId}`
   try {
     // Make a GET request to the page
     const response = await axios.get(url, { headers });
 
     // Load the HTML content into Cheerio
-    const $ = cheerio.load(response.data);
+    const $ = cheerio.load(response.data.html);
 
     // Find all div elements with the class 'fixed-img2'
     const fixedImg2Elements = $('.fixed-img2');
@@ -90,14 +91,42 @@ async function scrapePage(pageNumber) {
   }
 }
 
-// Function to iterate through the specified range of pages
-async function scrapePages() {
-  console.log('Scraping started');
-  for (let page = startPage; page <= endPage; page++) {
-    await scrapePage(page);
+async function getTopicId(){
+  const url = `${baseUrl}${section}`;
+
+  try {
+    // Make a GET request to the page
+    const response = await axios.get(url, { headers });
+
+    // Load the HTML content into Cheerio
+    const $ = cheerio.load(response.data);
+    
+    // Find the meta tag with property 'rs:topic-id'
+    const metaTag = $('meta[property="rs:topic-id"]');
+
+    // Extract topic_id from the content attribute
+    const topicId = metaTag.attr('content');
+
+    // Check if the topic_id is found
+    if (!topicId) {
+      console.error('Topic ID not found');
+      process.exit(1);
+    }
+    return topicId;
+  } catch (error) {
+    console.error(`Error during scraping:`, error.message);
   }
-  console.log('Scraping complete');
 }
 
-// Start scraping the pages
-scrapePages();
+  // Function to iterate through the specified range of pages
+  async function scrapePages() {
+    console.log('Scraping started');
+    const topicId = await getTopicId();
+    for (let page = startPage; page <= endPage; page++) {
+      await scrapePage(topicId, page);
+    }
+    console.log('Scraping complete');
+  }
+
+  // Start scraping the pages
+  scrapePages();
